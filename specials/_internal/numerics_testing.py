3a8afa4e77e8bc15a5b4aae80827c565ba0a0c1d
Leandro Lacerda Campos <15185896+leandrolcampos@users.noreply.github.com>
2024-02-24 13:39:13 -0300

    Refactor the `numerical_testing` module

--- specials/_internal/numerics_testing.py
+++ specials/_internal/numerics_testing.py
@@ -27,153 +27,172 @@
 # Springer International Publishing.
 # https://doi.org/10.1007/978-3-319-64110-2
 #
-# Brown, B. W., & Levy, L. B. (1994). Certification of Algorithm 708:
-#   significant-digit computation of the incomplete beta.
-# ACM Transactions on Mathematical Software (TOMS), 20(3), 393-397.
-# https://dl.acm.org/doi/10.1145/192115.192155
+# Muller, J. M. (2005). On the definition of ulp (x).
+# [Research Report] Laboratoire de l’informatique duparallélisme. 2005, 2+11p.
+# https://inria.hal.science/inria-00070503/file/RR2005-09.pdf
 
 """Numerical testing utilities."""
 
 import numpy as np
 
 
+def _promote_dtype(dtype):
+    """Promotes `dtype` to a higher precision floating-point type."""
+    dtype = np.dtype(dtype)
+
+    assert dtype in (np.float32, np.float64)
+
+    if dtype == np.float32:
+        return np.float64
+
+    return np.longdouble
+
+
 def py_relative_error(result, truth):
     """Computes the relative error of `result` relative to `truth`.
 
     The relative error is defined as `abs((result - truth) / truth)`. The computation
-    of that difference and ratio are done in 64-bit precision.
+    of that difference and ratio are done in the next higher precision compared to the
+    working precision.
+
+    The data types `result.dtype` and `truth.dtype` must correspond to the working
+    precision and a higher precision floating-point types, respectively.
 
     Args:
-        result: Array of values whose deviation to assess.
-        truth: Array of values presumed correct. Must broadcast with `result`.
+        result: A NumPy array of values whose deviation to assess.
+        truth: A NumPy Array of values presumed correct. Must broadcast with `result`.
 
     Returns:
-        Float64 array of elementwise relative error values.
+        A NumPy array of element-wise relative error values. The data type of the
+        output array is the next higher precision floating-point type compared to
+        the data type of `result`:
+            - `np.float64` if `result.dtype` is `np.float32`; or
+            - `np.longdouble` if `result.dtype` is `np.float64`.
+
+    Raises:
+        TypeError: If `result.dtype` is not a floating-point of single (`float32`) or
+            double (`float64`) precision.
+        TypeError: If `truth.dtype` is a floating-point type but does not have higher
+            precision than `result.dtype`.
     """
-    result = np.array(result, dtype=np.float64)
-    truth = np.array(truth, dtype=np.float64)
+    result = np.array(result)
+
+    if result.dtype not in (np.float32, np.float64):
+        raise TypeError(
+            "`result.dtype` must be a floating-point of single (`float32`) or double "
+            "(`float64`) precision"
+        )
+
+    output_dtype = _promote_dtype(result.dtype)
+    truth = np.array(truth)
+
+    if truth.dtype.kind == "f":
+        if np.finfo(truth.dtype).precision <= np.finfo(result.dtype).precision:
+            raise TypeError(
+                "When `truth.dtype` is a floating-point type, it must have higher "
+                "precision than `result.dtype`"
+            )
+
+    result = result.astype(output_dtype)
+    truth = truth.astype(output_dtype)
 
     truth_is_zero = np.equal(truth, 0.0)
-    safe_truth = np.where(truth_is_zero, np.ones_like(truth), truth)
+    largest_subnormal = np.nextafter(np.finfo(truth.dtype).smallest_normal, 0.0)
+    truth_is_subnormal = ~truth_is_zero & (np.abs(truth) <= largest_subnormal)
 
-    relerr = np.abs((result - truth) / safe_truth)
+    safe_denominator = np.where(
+        truth_is_zero,
+        np.ones_like(truth),
+        np.where(
+            (result > 0) & (truth > 0) & truth_is_subnormal, largest_subnormal, truth
+        ),
+    )
+
+    relerr = np.abs((result - truth) / safe_denominator)
     relerr = np.where(truth_is_zero, np.inf, relerr)
     relerr = np.where(result == truth, np.zeros_like(truth), relerr)
 
     return relerr
 
 
-def py_accuracy_in_significant_digits(relerr):
-    """Computes the number of significant digits of accuracy from the relative error.
-
-    The number of significant digits of accuracy is defined as `-log10(2 * relerr)`,
-    which is the usual definition: `n` significant digits of accuracy in a value allows
-    an error of `5` in the `(n + l)`st decimal place.
-
-    To avoid taking the logarithm of `0`, the relative error is bounded below by the
-    `epsneg` value of `float64`: the smallest positive floating-point number such that
-    `1.0 - epsneg != 1.0`.
-
-    The computation of this metric is done in 64-bit precision.
-
-    Args:
-        relerr: Array of relative error values.
-
-    Returns:
-        Float64 array of elementwise significant digits of accuracy.
-    """
-    relerr = np.array(relerr, dtype=np.float64)
-    return -np.log10(2.0 * np.maximum(relerr, np.finfo(np.float64).epsneg))
-
-
-def py_kahan_ulp(x, output_dtype):
+def py_kahan_ulp(x, target_dtype):
     """Computes the Kahan-ulp function at `x` element-wise.
 
     Ulp stands for "unit in the last place".
 
-    If `x` is a floating-point number, the Kahan-ulp at `x` is the distance between
-    the two floating-point numbers nearest `x`, even if `x` is not contained in the
-    interval between them.
+    If `x` is an arbitrary real number, the Kahan-ulp at `x` is the distance between the
+    two floating-point numbers nearest `x`, even if `x` is not contained in the interval
+    between them.
+
+    The computation of the Kahan-ulp is done in the next higher precision compared to the
+    working precision.
 
     Args:
-        x: Array of floating-point values.
-        output_dtype: Data type of the output array.
+        x: Array of floating-point values. The data type of this array must have higher
+            precision than `target_dtype`.
+        target_dtype: Data type corresponding to the working precision.
 
     Returns:
-        Array of element-wise Kahan-ulp values.
+        Array of element-wise Kahan-ulp values. The data type of the output array is the
+        next higher precision floating-point type compared to the data type of `x`:
+            - `np.float64` if `result.dtype` is `np.float32`; or
+            - `np.longdouble` if `result.dtype` is `np.float64`.
 
     Raises:
-        TypeError: If `x.dtype` or `output_dtype` is not a floating-point type.
-        TypeError: If `output_dtype` has higher precision than `x.dtype`.
+        TypeError: If `target_dtype` is not a floating-point of single (`float32`) or
+            double (`float64`) precision.
+        TypeError: If `x.dtype` is a floating-point type but does not have higher
+            precision than `target_dtype`.
     """
+    target_dtype = np.dtype(target_dtype)
+    if target_dtype not in (np.float32, np.float64):
+        raise TypeError(
+            "`target_dtype` must be a floating-point of single (`float32`) or double "
+            "(`float64`) precision"
+        )
+
     x = np.array(x)
+    if x.dtype.kind == "f":
+        if np.finfo(x.dtype).precision <= np.finfo(target_dtype).precision:
+            raise TypeError(
+                "When `x.dtype` is a floating-point type, it must have higher "
+                "precision than `target_dtype`"
+            )
 
-    dtype = x.dtype
-    output_dtype = np.dtype(output_dtype)
+    higher_precision_dtype = _promote_dtype(target_dtype)
+    x = x.astype(higher_precision_dtype)
 
-    if dtype.kind != "f":
-        raise TypeError("`x.dtype` must be a floating-point type")
+    x = np.abs(x)
+    one = higher_precision_dtype(1)
 
-    if output_dtype.kind != "f":
-        raise TypeError("`output_dtype` must be a floating-point type")
+    emax = np.finfo(target_dtype).maxexp
+    emin = np.finfo(target_dtype).minexp + 1
+    t = np.finfo(target_dtype).nmant
+    xsml = np.ldexp(one, emin)
+    xmax = np.finfo(target_dtype).max
 
-    if np.finfo(output_dtype).precision > np.finfo(dtype).precision:
-        raise TypeError("`output_dtype` cannot have higher precision than `x.dtype`")
-
-    dtype = dtype.type
-    output_dtype = output_dtype.type
-
-    xmin = np.finfo(output_dtype).smallest_normal
-    xeps = np.finfo(output_dtype).eps
-    emax = np.finfo(output_dtype).maxexp - 1
-    emin = np.finfo(output_dtype).minexp - 1
-    t = np.finfo(output_dtype).nmant
-    esub = emin - t + 1
-    one = dtype(1.0)
+    is_normal = (x >= xsml) & (x <= xmax)
 
     result = np.empty_like(x)
-    result = np.where(np.isnan(x.astype(output_dtype)), np.nan, result)
-    result = np.where(
-        np.isinf(x.astype(output_dtype)), np.ldexp(one, emax - t + 1), result
+    result = np.where(np.isnan(x), np.nan, result)
+    result = np.where(x < xsml, np.ldexp(one, emin - (t + 1)), result)
+    result = np.where(x > xmax, np.ldexp(one, emax - (t + 1)), result)
+
+    safe_x = np.where(is_normal, x, one)
+    expmin = np.log2(safe_x).astype(np.int32)
+    exponent = expmin - t
+
+    powermin = np.ldexp(one, expmin)
+    exponent = np.where(
+        safe_x / powermin <= one + np.ldexp(one, -(t + 2)), exponent - 1, exponent
     )
-    result = np.where(np.abs(x) <= xmin, np.ldexp(one, esub), result)
 
-    is_main_branch = np.isfinite(x.astype(output_dtype)) & (np.abs(x) > xmin)
-    y = np.where(is_main_branch, x, one)
-
-    _, e = np.frexp(y)
-    s = np.ldexp(np.abs(y), -e)
-    e -= t - 1
-
-    e = np.where((s - one) < (0.5 * xeps), e - 1, e)
-    e = np.maximum(e, esub)
-    result = np.where(is_main_branch, np.ldexp(one, e), result)
-
-    result = result.astype(output_dtype)
-    result = np.where(result == 0.0, xmin, result)
+    result = np.where(is_normal, np.ldexp(one, exponent), result)
 
     if result.shape == ():
-        return output_dtype(result.item(0))
+        return higher_precision_dtype(result.item(0))
 
     return result
-
-
-def _promote_dtype(dtype):
-    """Promotes `dtype` to a higher precision floating-point type."""
-    dtype = np.dtype(dtype)
-    if dtype.kind != "f":
-        raise TypeError("`dtype` must be a floating-point type")
-
-    if np.finfo(dtype).bits < 32:
-        return np.float32
-
-    if np.finfo(dtype).bits < 64:
-        return np.float64
-
-    if np.finfo(dtype).bits < 128:
-        return np.float128
-
-    return dtype
 
 
 def py_error_in_ulps(result, truth):
@@ -181,36 +200,52 @@ def py_error_in_ulps(result, truth):
 
     Ulp stands for "unit in the last place".
 
-    The error in ulps is defined as `abs((result - truth) / kahan_ulp(truth))`. See the
-    `kahan_ulp` function for more details.
+    The error in ulps is defined as `abs((result - truth) / kahan_ulp(truth))`. The
+    computation of that difference and ratio are done in the next higher precision
+    compared to the working precision.
 
-    If `truth.dtype` is not a floating-point type, it is cast to the following type:
-        - `np.float32` if `result.dtype.bits < 32`;
-        - `np.float64` if `32 <= result.dtype.bits < 64`;
-        - `np.float128` if `64 <= result.dtype.bits < 128`; or
-        - `result.dtype` otherwise.
+    The data types `result.dtype` and `truth.dtype` must correspond to the working
+    precision and a higher precision floating-point types, respectively.
 
     Args:
         result: Array of values whose deviation to assess.
         truth: Array of values presumed correct. Must broadcast with `result`.
 
     Returns:
-        Array of element-wise error in ulps values.
+        A NumPy array of element-wise error in ulps values. The data type of the
+        output array is the next higher precision floating-point type compared to
+        the data type of `result`:
+            - `np.float64` if `result.dtype` is `np.float32`; or
+            - `np.longdouble` if `result.dtype` is `np.float64`.
 
     Raises:
-        TypeError: If `result.dtype` is not a floating-point type.
-        TypeError: If `result.dtype` has higher precision than `truth.dtype`.
+        TypeError: If `result.dtype` is not a floating-point of single (`float32`) or
+            double (`float64`) precision.
+        TypeError: If `truth.dtype` is a floating-point type but does not have higher
+            precision than `result.dtype`.
     """
     result = np.array(result)
 
-    if result.dtype.kind != "f":
-        raise TypeError("`result.dtype` must be a floating-point type")
+    if result.dtype not in (np.float32, np.float64):
+        raise TypeError(
+            "`result.dtype` must be a floating-point of single (`float32`) or double "
+            "(`float64`) precision"
+        )
 
+    target_dtype = result.dtype
+    output_dtype = _promote_dtype(target_dtype)
     truth = np.array(truth)
 
-    if truth.dtype.kind != "f":
-        truth = truth.astype(_promote_dtype(result.dtype))
+    if truth.dtype.kind == "f":
+        if np.finfo(truth.dtype).precision <= np.finfo(result.dtype).precision:
+            raise TypeError(
+                "When `truth.dtype` is a floating-point type, it must have higher "
+                "precision than `result.dtype`"
+            )
 
-    ulp_truth = py_kahan_ulp(truth, output_dtype=result.dtype)
+    result = result.astype(output_dtype)
+    truth = truth.astype(output_dtype)
 
-    return np.abs((result - truth).astype(result.dtype) / ulp_truth)
+    ulp_truth = py_kahan_ulp(truth, target_dtype=target_dtype)
+
+    return np.abs((result - truth) / ulp_truth)
